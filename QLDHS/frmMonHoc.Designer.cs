fef30078e0d731f62d278e8fe2b90e64a32907f8
vanhaust1134 <73728453+vanhaust1134@users.noreply.github.com>
2021-12-04 14:58:41 +0700

    vvv

--- QLDHS/frmMonHoc.Designer.cs
+++ QLDHS/frmMonHoc.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmMonHoc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(646, 374);
+            this.Name = "frmMonHoc";
             this.Text = "frmMonHoc";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
